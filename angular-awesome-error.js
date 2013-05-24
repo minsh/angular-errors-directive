673e62ea08ab218a53fbeeb679189cb1e79f8146
Rabi C Shah <green.rabi@hotmail.com>
2013-05-24 13:31:14 +0530

    Errors modified

--- angular-awesome-error.js
+++ angular-awesome-error.js
@@ -30,15 +30,26 @@
                   </div>',
          link: function(scope) {
           scope.errorMessage = {
-            "INVALID_EMAIL_OR_PASSWORD" : "Sorry, Invalid Email or Password.",
+  "INVALID_EMAIL_OR_PASSWORD" : "Sorry, Invalid Email or Password.",
             "USERNAME_OR_PASSWORD_NOT_CORRECT" : "Sorry, Username or Password is not correct.",
             "USER_ALREADY_EXISTS" : "Sorry, User already exists.",
-            "MISSING_NICKNAME_PARAMETER" : "Sorry, Missing Nickname",
+            "MISSING_USERNAME_PARAMETER" : "Sorry, Missing Username",
             "MISSING_PASSWORD_PARAMETER" : "Sorry, Missing Password.",
             "MISSING_USERNAME" : "Sorry, Missing Username.",
             "INVALID_EMAIL" : "Sorry, Not a valid Email.",
-            "EMAIL_REQUIRED" : "Sorry, Email required"
-          };
+            "EMAIL_REQUIRED" : "Sorry, Email required",
+            "USERNAME_PATTERN_ERROR" : "Username should contain alphabets and number length between 3-15",
+            "EMAIL_PATTERN_ERROR" : "Not a valid email", 
+            "PASSWORD_LENGTH_ERROR" : "Password length shouble be between 7 and 20",
+            "useremail_REQUIRED" : "Email Required",
+            "username_REQUIRED" : "Username Required",
+            "password_REQUIRED" : "Password Required",
+            "useremail_INVALID" : "Invalid Email",
+            "username_INVALID_PATTERN" : "Username should contain alphabets and number length between 3-15",
+            "email_INVALID" : "Invalid Email",
+            "email_REQUIRED" : "Email Required",
+            "password_LENGTH_PATTERN" : "Password length shouble be between 7 and 20",
+            "USERNAME_ALREADY_TAKEN" : "Sorry, Username already taken"          };
         
           scope.$on('show error', function (args) {
             scope.showerr = true;
