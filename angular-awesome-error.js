453c175dc985dfbbc80958155249eb5dec45b1c3
Rabi C Shah <rabi@minsh.net>
2013-09-26 17:39:32 +0530

    Error messages independent from this directive

--- angular-awesome-error.js
+++ angular-awesome-error.js
@@ -1,14 +1,22 @@
-/* 
+/*
  * Template for error/success message,
- * Show/hide error message when required, 
+ * Show/hide error message when required,
  * Show/hide success message when required.
  */
 
 (function(window, angular, undefined) {
 
   'use strict';
+
   angular.module('ngAngularError', ['ng'])
-    .directive('awesomeError', function () {
+    .factory('errorsList', function($http) {
+      /* errors_list.json should contain a map of errors and user-feedback.
+       * we use, $http to retrieve values from file errors_list.json,
+       * errorsList factory returns a promise. */
+      return $http.get('errors_list.json');
+    })
+
+    .directive('awesomeError', function (errorsList) {
       return {
         restrict: 'E',
         replace: true,
@@ -27,55 +35,14 @@
                       </div> \
                     </div> \
                    </div>',
-         link: function(scope) {
-          scope.errorMessage = {
-            "USERNAME_OR_PASSWORD_NOT_CORRECT": "Invalid username or password.",
-            "USERNAME_OR_PASSWORD_OR_TOKEN_NOT_CORRECT" : "Invalid username or password.",
-            "MISSING_USERNAME_OR_EMAIL" : "Please, enter a username or email.",
-            "INVALID_USERNAME_OR_EMAIL" : "Invalid username or email.",
-            "MISSING_USERNAME" : "Please, enter a username.",
-            "INVALID_USERNAME" : "Invalid username.",
-            "USERNAME_NOT_AVAILABLE": "This username is already taken!",
-            "USERNAME_ALREADY_TAKEN": "This username is already taken!",
-            "username_REQUIRED": "Please, enter a username.",
-            "username_INVALID_PATTERN" : "Username should be composed of alphanumerics only and 1 to 15 characters long.",
-            "USERNAME_UPDATED" : "Username successfully updated.",
+        link: function(scope) {
+          scope.errorMessage = {};
 
-            "MISSING_PASSWORD" : "Please, enter a password.",
-            "INVALID_PASSWORD" : "Invalid password.",
-            "password_REQUIRED" : "Please, enter a password.",
-            "password_LENGTH_PATTERN" : "Password should be 3 to 15 characters long",
-            "PASSWORDS_DONT_MATCH" : "Passwords dont match.",
-            "PASSWORD_UPDATED" : "Password successfully updated.",
+          /* fetching data present in 'errors-list.json' file */
+          errorsList.success(function(data) {
+            scope.errorMessage = data;
+          });
 
-            "email_INVALID" : "Invalid email.",
-            "email_REQUIRED" : "Please, enter an email.",
-            "MISSING_EMAIL" : "Please, enter an email.",
-            "EMAIL_NOT_AVAILABE" : "This email is already taken!",
-            "useremail_REQUIRED" :  "Please, enter an email.",
-            "useremail_INVALID" : "Invalid email",
-            "MAIL_SENT_RESET_PASSWORD" : "Email with password reset instructions has been sent to your email address",
-
-            "TOKEN_NOT_VALID" : "Oops, something went wrong. Please, try again later.",
-            "MISSING_TOKEN" : "Oops, something went wrong. Please, try again later.",
-            "INVALID_TOKEN" : "Oops, something went wrong. Please, try again later.",
-            "TOKEN_NOT_CORRECT" : "Oops, something went wrong. Please, try again later.",
-            "TOKEN_EXPIRED" : "Your session has expired. Please, try to login again.",
-
-            "MISSING_GEO" : "We are not able to get your location. Please, try again.",
-            "INVALID_GEO" : "We are not able to get your location. Please, try again.",
-
-            "FILE_NOT_VALID" : "File not valid",
-            "USER_NOT_FOUND" : "This user was not found, we apologise for the inconvenience.",
-            "SHOUT_NOT_FOUND" : "This message was not found, we apologise for the inconvenience.",
-            "MISSING_SHOUT" : "Please, enter a message.",
-            "INVALID_SHOUT" : "Invalid message.",
-            "INVALID_IN_REPLY_TO" : "Invalid reply message.",
-            "FEEDBACK_RESPONSE" : "Thank you for your feedback.",
-            "FEEDBACK_ERROR_RESPONSE" : "Sorry the feedback could not be sent.",
-            "FEEDBACK_MISSING" : "Please, enter a feedback.",
-          };
-        
           scope.$on('show error', function (args) {
             scope.showerr = true;
           });
@@ -100,7 +67,7 @@
              scope.showMessage = scope.errorMessage[scope.msg];
              if (scope.showMessage === "undefined") { scope.showMessage = scope.errorMessage["Undefined"]; }
           });
-         }
+        }
       };
     });
 
