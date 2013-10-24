2129c8f14c152dd613d33ab112002a75147bd074
SKatiyar <katiyar.suyash@gmail.com>
2013-10-24 16:32:19 +0530

    awesome error updated

--- README.md
+++ README.md
@@ -9,18 +9,20 @@ Usage
 * **Requires Twitter Bootstrap**
 
 * Include the directive in your template as,
-*      <awesome-error msg="errorMessage"></awesome-error> where, errorMessage is in the scope
+*      <awesome-error target="'settingsPage'"></awesome-error> where, target defines the error directive you are targetting.
 * Then, broadcast or emit the message from controllers or directives. e.g.
-*      $scope.$emit('show error');      // to show error msg
-*      $scope.$emit('hide error');      // to hide error msg
-*      $scope.$emit('show success');    // to show success msg
-*      $scope.$emit('hide success');    // to hide success msg
+*      $scope.$broadcast(errorState, target, error);                         // template of broadcast
+*      $scope.$broadcast('show error', 'settingsPage', 'INVALID_DATE');      // to show error msg
+*      $scope.$emit('hide error', 'settingsPage');                           // to hide error msg
+*      $scope.$emit('show success', 'settingsPage', 'TWEET_POSTED');         // to show success msg
+*      $scope.$emit('hide success', 'settingsPage');                         // to hide success msg
 
 You need to keep all your error messages in a JSON file 'errors_list.json'. e.g.
 ```
 {
   "INVALID_DATE" : "Please enter a valid date",
-  "username_not_found" : "Sorry, This username is already taken"
+  "username_not_found" : "Sorry, This username is already taken",
+  "TWEET_POSTED": "Tweet was posted successfully"
 }
 ```
 
--- angular-awesome-error.js
+++ angular-awesome-error.js
@@ -9,19 +9,18 @@
   'use strict';
 
   angular.module('ngAngularError', ['ng'])
-    .factory('errorsList', function($http) {
+    .factory('list', function($http) {
       /* errors_list.json should contain a map of errors and user-feedback.
        * we use, $http to retrieve values from file errors_list.json,
        * errorsList factory returns a promise. */
       return $http.get('errors_list.json');
     })
-
-    .directive('awesomeError', function (errorsList) {
+    .directive('awesomeError', function (list) {
       return {
         restrict: 'E',
         replace: true,
-        scope : {
-          msg : '=msg'
+        scope: {
+          target: '='
         },
         template: '<div> \
                     <div ng-show="showerr"> \
@@ -36,43 +35,44 @@
                     </div> \
                    </div>',
         link: function(scope) {
-          scope.errorMessage = {};
 
-          /* fetching data present in 'errors-list.json' file */
-          errorsList.success(function(data) {
-            scope.errorMessage = data;
-          });
+          scope.messages = {};
 
-          scope.$on('show error', function (args) {
-            scope.showerr = true;
-          });
-
-          scope.$on('hide error', function (args) {
-            scope.showerr = false;
-          });
-
-          scope.hideError = function () {
-            scope.showerr = false;
+          var _showMsg = function(val, args, target, str) {
+            if (scope.target === target) {
+              scope.showMessage = scope.messages[str];
+              if (scope.showMessage === "") {
+                scope.showMessage = scope.messages["Undefined"];
+              }
+              if (args.name.indexOf('error') !== -1) {
+                scope.showerr = val;
+              } else {
+                scope.showsucc = val;
+              }
+            }
           };
 
-          scope.$on('show success', function (args) {
-            scope.showsucc = true;
+          /* fetching data present in 'errors-list.json' file */
+          list.success(function(data) {
+            scope.messages = data;
           });
 
-          scope.$on('hide success', function (args) {
-            scope.showsucc = false;
+          scope.$on('show error', function (args, target, err) {
+            _showMsg(true, args, target, err);
           });
 
-          scope.$watch(function() {return scope.msg}, function(value) {
-             scope.showMessage = scope.errorMessage[scope.msg];
-             if (scope.showMessage === "") {
-               scope.$apply(function() {
-                 scope.showMessage = scope.errorMessage["Undefined"];
-               });
-             }
+          scope.$on('hide error', function (args, target) {
+            _showMsg(false, args, target);
+          });
+
+          scope.$on('show success', function (args, target, succ) {
+            _showMsg(true, args, target, succ);
+          });
+
+          scope.$on('hide success', function (args, target) {
+            _showMsg(false, args, target);
           });
         }
       };
     });
-
 })(window, window.angular);
